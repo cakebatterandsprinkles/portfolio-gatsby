2aeb6782da4d7860db2101581e499eba3b1f8cd2
yagmurcetintas <yagmurcetin@gmail.com>
2020-08-11 07:15:04 -0400

    added media queries of portfolio page for smaller screens

--- src/pages/index.js
+++ src/pages/index.js
@@ -37,199 +37,211 @@ const PortfolioPage = () => {
         <div className={styles.portfolioWrapper}>
           <h1>Portfolio</h1>
           <div className={styles.gridContainer}>
-            <ReactCardFlip
-              isFlipped={isFlipped1}
-              flipDirection="vertical"
-              flipSpeedFrontToBack="2"
-              flipSpeedBackToFront="2"
-            >
-              <CardFront style="whereiscovid" onClick={ClickHandler1}>
-                <p>whereiscovid</p>
-              </CardFront>
-              <CardBack style="backside" onClick={ClickHandler1}>
-                <p className={styles.cardTitle}>whereiscovid</p>
-                <p className={styles.cardSummary}>
-                  A COVID19 tracking app. This app provides recent information
-                  about Coronavirus while visualizing data on Google Maps.
-                </p>
-                <hr className={styles.hr}></hr>
-                <div className={styles.cardWrapper}>
-                  <p className={styles.cardLinkTitle}>Demo: </p>
-                  <a
-                    href="https://whereiscovid.info/"
-                    target="_blank"
-                    rel="noreferrer"
-                    className={styles.cardlink}
-                  >
-                    https://whereiscovid.info/
-                  </a>
-                </div>
-                <div className={styles.cardWrapper}>
-                  <p className={styles.cardLinkTitle}>Github: </p>
-                  <a
-                    href="https://github.com/cakebatterandsprinkles/whereiscovid"
-                    target="_blank"
-                    rel="noreferrer"
-                    className={styles.cardlink}
-                  >
-                    cakebatterandsprinkles/whereiscovid
-                  </a>
-                </div>
-                <div className={styles.cardWrapper}>
-                  <p className={styles.cardLinkTitle}>
-                    Technologies used:{" "}
-                    <span id={styles.technologies}>
-                      React.js, react-router-dom, novelcovid API, pubmed API,
-                      GeoIP, Google Maps API
-                    </span>
+            <div className={styles.card}>
+              <ReactCardFlip
+                isFlipped={isFlipped1}
+                flipDirection="vertical"
+                flipSpeedFrontToBack="2"
+                flipSpeedBackToFront="2"
+                containerStyle={{ width: "100%", height: "100%" }}
+              >
+                <CardFront style="whereiscovid" onClick={ClickHandler1}>
+                  <p>whereiscovid</p>
+                </CardFront>
+                <CardBack style="backside" onClick={ClickHandler1}>
+                  <p className={styles.cardTitle}>whereiscovid</p>
+                  <p className={styles.cardSummary}>
+                    A COVID19 tracking app. This app provides recent information
+                    about Coronavirus while visualizing data on Google Maps.
                   </p>
-                </div>
-              </CardBack>
-            </ReactCardFlip>
-            <ReactCardFlip
-              isFlipped={isFlipped2}
-              flipDirection="vertical"
-              flipSpeedFrontToBack="2"
-              flipSpeedBackToFront="2"
-            >
-              <CardFront style="mendokusai" onClick={ClickHandler2}>
-                <p>mendokusai</p>
-              </CardFront>
-              <CardBack style="backside" onClick={ClickHandler2}>
-                <p className={styles.cardTitle}>mendokusai</p>
-                <p className={styles.cardSummary}>
-                  A todo list app for people who have problem planning their
-                  days. By dividing things you need to do in bite size pieces,
-                  you can have a more productive day and feel more motivated and
-                  satisfied at the end of the day.
-                </p>
-                <hr className={styles.hr}></hr>
-                <div className={styles.cardWrapper}>
-                  <p className={styles.cardLinkTitle}>Demo: </p>
-                  <a
-                    href="https://mendokusai.app/"
-                    target="_blank"
-                    rel="noreferrer"
-                    className={styles.cardlink}
-                  >
-                    https://mendokusai.app/
-                  </a>
-                </div>
-                <div className={styles.cardWrapper}>
-                  <p className={styles.cardLinkTitle}>Github: </p>
-                  <a
-                    href="https://github.com/cakebatterandsprinkles/mendokusai-react"
-                    target="_blank"
-                    rel="noreferrer"
-                    className={styles.cardlink}
-                  >
-                    cakebatterandsprinkles/mendokusai-react
-                  </a>
-                </div>
-                <div className={styles.cardWrapper}>
-                  <p className={styles.cardLinkTitle}>
-                    Technologies used:{" "}
-                    <span id={styles.technologies}>
-                      React.js react-router-dom react-redux OpenWeatherMapAPI
-                      GeoIP Express.js MongoDB-Atlas Sendgrid Heroku
-                    </span>
+                  <hr className={styles.hr}></hr>
+                  <div className={styles.cardWrapper}>
+                    <p className={styles.cardLinkTitle}>Demo: </p>
+                    <a
+                      href="https://whereiscovid.info/"
+                      target="_blank"
+                      rel="noreferrer"
+                      className={styles.cardlink}
+                    >
+                      https://whereiscovid.info/
+                    </a>
+                  </div>
+                  <div className={styles.cardWrapper}>
+                    <p className={styles.cardLinkTitle}>Github: </p>
+                    <a
+                      href="https://github.com/cakebatterandsprinkles/whereiscovid"
+                      target="_blank"
+                      rel="noreferrer"
+                      className={styles.cardlink}
+                    >
+                      cakebatterandsprinkles/whereiscovid
+                    </a>
+                  </div>
+                  <div className={styles.cardWrapper}>
+                    <p className={styles.cardLinkTitle}>
+                      Technologies used:{" "}
+                      <span id={styles.technologies}>
+                        React.js, react-router-dom, novelcovid API, pubmed API,
+                        GeoIP, Google Maps API
+                      </span>
+                    </p>
+                  </div>
+                </CardBack>
+              </ReactCardFlip>
+            </div>
+            <div className={styles.card}>
+              <ReactCardFlip
+                isFlipped={isFlipped2}
+                flipDirection="vertical"
+                flipSpeedFrontToBack="2"
+                flipSpeedBackToFront="2"
+                containerStyle={{ width: "100%", height: "100%" }}
+              >
+                <CardFront style="mendokusai" onClick={ClickHandler2}>
+                  <p>mendokusai</p>
+                </CardFront>
+                <CardBack style="backside" onClick={ClickHandler2}>
+                  <p className={styles.cardTitle}>mendokusai</p>
+                  <p className={styles.cardSummary}>
+                    A todo list app for people who have problem planning their
+                    days. By dividing things you need to do in bite size pieces,
+                    you can have a more productive day and feel more motivated
+                    and satisfied at the end of the day.
                   </p>
-                </div>
-              </CardBack>
-            </ReactCardFlip>
-            <ReactCardFlip
-              isFlipped={isFlipped3}
-              flipDirection="vertical"
-              flipSpeedFrontToBack="2"
-              flipSpeedBackToFront="2"
-            >
-              <CardFront style="mockingbird" onClick={ClickHandler3}>
-                <p>mockingbird</p>
-              </CardFront>
-              <CardBack style="backside" onClick={ClickHandler3}>
-                <p className={styles.cardTitle}>mockingbird</p>
-                <p className={styles.cardSummary}>
-                  A 7 minute journal app for well-being.
-                </p>
-                <hr className={styles.hr}></hr>
-                <div className={styles.cardWrapper}>
-                  <p className={styles.cardLinkTitle}>Demo: </p>
-                  <a
-                    href="https://mendokusai.app/"
-                    target="_blank"
-                    rel="noreferrer"
-                    className={styles.cardlink}
-                  >
-                    https://mendokusai.app/
-                  </a>
-                </div>
-                <div className={styles.cardWrapper}>
-                  <p className={styles.cardLinkTitle}>Github: </p>
-                  <a
-                    href="https://github.com/cakebatterandsprinkles/mockingbird"
-                    target="_blank"
-                    rel="noreferrer"
-                    className={styles.cardlink}
-                  >
-                    cakebatterandsprinkles/mockingbird
-                  </a>
-                </div>
-                <div className={styles.cardWrapper}>
-                  <p className={styles.cardLinkTitle}>
-                    Technologies used:{" "}
-                    <span id={styles.technologies}>
-                      React.js react-router-dom react-redux Express.js
-                      MongoDB-Atlas Sendgrid Heroku
-                    </span>
+                  <hr className={styles.hr}></hr>
+                  <div className={styles.cardWrapper}>
+                    <p className={styles.cardLinkTitle}>Demo: </p>
+                    <a
+                      href="https://mendokusai.app/"
+                      target="_blank"
+                      rel="noreferrer"
+                      className={styles.cardlink}
+                    >
+                      https://mendokusai.app/
+                    </a>
+                  </div>
+                  <div className={styles.cardWrapper}>
+                    <p className={styles.cardLinkTitle}>Github: </p>
+                    <a
+                      href="https://github.com/cakebatterandsprinkles/mendokusai-react"
+                      target="_blank"
+                      rel="noreferrer"
+                      className={styles.cardlink}
+                    >
+                      cakebatterandsprinkles/mendokusai-react
+                    </a>
+                  </div>
+                  <div className={styles.cardWrapper}>
+                    <p className={styles.cardLinkTitle}>
+                      Technologies used:{" "}
+                      <span id={styles.technologies}>
+                        React.js react-router-dom react-redux OpenWeatherMapAPI
+                        GeoIP Express.js MongoDB-Atlas Sendgrid Heroku
+                      </span>
+                    </p>
+                  </div>
+                </CardBack>
+              </ReactCardFlip>
+            </div>
+            <div className={styles.card}>
+              <ReactCardFlip
+                isFlipped={isFlipped3}
+                flipDirection="vertical"
+                flipSpeedFrontToBack="2"
+                flipSpeedBackToFront="2"
+                containerStyle={{ width: "100%", height: "100%" }}
+              >
+                <CardFront style="mockingbird" onClick={ClickHandler3}>
+                  <p>mockingbird</p>
+                </CardFront>
+                <CardBack style="backside" onClick={ClickHandler3}>
+                  <p className={styles.cardTitle}>mockingbird</p>
+                  <p className={styles.cardSummary}>
+                    A 7 minute journal app for well-being.
                   </p>
-                </div>
-              </CardBack>
-            </ReactCardFlip>
-            <ReactCardFlip
-              isFlipped={isFlipped4}
-              flipDirection="vertical"
-              flipSpeedFrontToBack="2"
-              flipSpeedBackToFront="2"
-            >
-              <CardFront style="breakout" onClick={ClickHandler4}>
-                <p>breakout</p>
-              </CardFront>
-              <CardBack style="backside" onClick={ClickHandler4}>
-                <p className={styles.cardTitle}>breakout</p>
-                <p className={styles.cardSummary}>
-                  Atari Breakout game remade with HTML5 Canvas API.
-                </p>
-                <hr className={styles.hr}></hr>
-                <div className={styles.cardWrapper}>
-                  <p className={styles.cardLinkTitle}>Demo: </p>
-                  <a
-                    href="https://cakebatterandsprinkles.github.io/breakout/"
-                    target="_blank"
-                    rel="noreferrer"
-                    className={styles.cardlink}
-                  >
-                    https://cakebatterandsprinkles.github.io/breakout/
-                  </a>
-                </div>
-                <div className={styles.cardWrapper}>
-                  <p className={styles.cardLinkTitle}>Github: </p>
-                  <a
-                    href="https://github.com/cakebatterandsprinkles/breakout"
-                    target="_blank"
-                    rel="noreferrer"
-                    className={styles.cardlink}
-                  >
-                    cakebatterandsprinkles/breakout
-                  </a>
-                </div>
-                <div className={styles.cardWrapper}>
-                  <p className={styles.cardLinkTitle}>
-                    Technologies used:{" "}
-                    <span id={styles.technologies}>HTML5 Canvas API</span>
+                  <hr className={styles.hr}></hr>
+                  <div className={styles.cardWrapper}>
+                    <p className={styles.cardLinkTitle}>Demo: </p>
+                    <a
+                      href="https://mendokusai.app/"
+                      target="_blank"
+                      rel="noreferrer"
+                      className={styles.cardlink}
+                    >
+                      https://mendokusai.app/
+                    </a>
+                  </div>
+                  <div className={styles.cardWrapper}>
+                    <p className={styles.cardLinkTitle}>Github: </p>
+                    <a
+                      href="https://github.com/cakebatterandsprinkles/mockingbird"
+                      target="_blank"
+                      rel="noreferrer"
+                      className={styles.cardlink}
+                    >
+                      cakebatterandsprinkles/mockingbird
+                    </a>
+                  </div>
+                  <div className={styles.cardWrapper}>
+                    <p className={styles.cardLinkTitle}>
+                      Technologies used:{" "}
+                      <span id={styles.technologies}>
+                        React.js react-router-dom react-redux Express.js
+                        MongoDB-Atlas Sendgrid Heroku
+                      </span>
+                    </p>
+                  </div>
+                </CardBack>
+              </ReactCardFlip>
+            </div>
+            <div className={styles.card}>
+              <ReactCardFlip
+                isFlipped={isFlipped4}
+                flipDirection="vertical"
+                flipSpeedFrontToBack="2"
+                flipSpeedBackToFront="2"
+                containerStyle={{ width: "100%", height: "100%" }}
+              >
+                <CardFront style="breakout" onClick={ClickHandler4}>
+                  <p>breakout</p>
+                </CardFront>
+                <CardBack style="backside" onClick={ClickHandler4}>
+                  <p className={styles.cardTitle}>breakout</p>
+                  <p className={styles.cardSummary}>
+                    Atari Breakout game remade with HTML5 Canvas API.
                   </p>
-                </div>
-              </CardBack>
-            </ReactCardFlip>
+                  <hr className={styles.hr}></hr>
+                  <div className={styles.cardWrapper}>
+                    <p className={styles.cardLinkTitle}>Demo: </p>
+                    <a
+                      href="https://cakebatterandsprinkles.github.io/breakout/"
+                      target="_blank"
+                      rel="noreferrer"
+                      className={styles.cardlink}
+                    >
+                      cakebatterandsprinkles.github.io/breakout/
+                    </a>
+                  </div>
+                  <div className={styles.cardWrapper}>
+                    <p className={styles.cardLinkTitle}>Github: </p>
+                    <a
+                      href="https://github.com/cakebatterandsprinkles/breakout"
+                      target="_blank"
+                      rel="noreferrer"
+                      className={styles.cardlink}
+                    >
+                      cakebatterandsprinkles/breakout
+                    </a>
+                  </div>
+                  <div className={styles.cardWrapper}>
+                    <p className={styles.cardLinkTitle}>
+                      Technologies used:{" "}
+                      <span id={styles.technologies}>HTML5 Canvas API</span>
+                    </p>
+                  </div>
+                </CardBack>
+              </ReactCardFlip>
+            </div>
           </div>
         </div>
       </div>
