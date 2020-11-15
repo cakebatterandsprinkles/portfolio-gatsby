161d7f3dda32cfd3d903d0c7c19c3ec5e7f5f82d
yagmurcetintas <yagmurcetin@gmail.com>
2020-11-15 17:50:24 -0500

    fixed card styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -45,45 +45,42 @@ const PortfolioPage = () => {
                   <p>whereiscovid</p>
                 </CardFront>
                 <CardBack cardStyle="backside" onClick={ClickHandler1}>
-                  <div className={styles.cardBackContainer}>
-                    <p className={styles.cardTitle}>whereiscovid</p>
-                    <p className={styles.cardSummary}>
-                      A COVID19 tracking app. This app provides recent
-                      information about Coronavirus while visualizing data on
-                      Google Maps.
+                  <p className={styles.cardTitle}>whereiscovid</p>
+                  <p className={styles.cardSummary}>
+                    A COVID19 tracking app. This app provides recent information
+                    about Coronavirus while visualizing data on Google Maps.
+                  </p>
+                  <hr className={styles.hr}></hr>
+                  <div className={styles.cardWrapper}>
+                    <p className={styles.cardLinkTitle}>Demo: </p>
+                    <a
+                      href="https://whereiscovid.info/"
+                      target="_blank"
+                      rel="noreferrer"
+                      className={styles.cardlink}
+                    >
+                      https://whereiscovid.info/
+                    </a>
+                  </div>
+                  <div className={styles.cardWrapper}>
+                    <p className={styles.cardLinkTitle}>Github: </p>
+                    <a
+                      href="https://github.com/cakebatterandsprinkles/whereiscovid"
+                      target="_blank"
+                      rel="noreferrer"
+                      className={styles.cardlink}
+                    >
+                      cakebatterandsprinkles/whereiscovid
+                    </a>
+                  </div>
+                  <div className={styles.cardWrapper}>
+                    <p className={styles.cardLinkTitle}>
+                      Technologies used:{" "}
+                      <span id={styles.technologies}>
+                        React.js, react-router-dom, novelcovid API, pubmed API,
+                        GeoIP, Google Maps API
+                      </span>
                     </p>
-                    <hr className={styles.hr}></hr>
-                    <div className={styles.cardWrapper}>
-                      <p className={styles.cardLinkTitle}>Demo: </p>
-                      <a
-                        href="https://whereiscovid.info/"
-                        target="_blank"
-                        rel="noreferrer"
-                        className={styles.cardlink}
-                      >
-                        https://whereiscovid.info/
-                      </a>
-                    </div>
-                    <div className={styles.cardWrapper}>
-                      <p className={styles.cardLinkTitle}>Github: </p>
-                      <a
-                        href="https://github.com/cakebatterandsprinkles/whereiscovid"
-                        target="_blank"
-                        rel="noreferrer"
-                        className={styles.cardlink}
-                      >
-                        cakebatterandsprinkles/whereiscovid
-                      </a>
-                    </div>
-                    <div className={styles.cardWrapper}>
-                      <p className={styles.cardLinkTitle}>
-                        Technologies used:{" "}
-                        <span id={styles.technologies}>
-                          React.js, react-router-dom, novelcovid API, pubmed
-                          API, GeoIP, Google Maps API
-                        </span>
-                      </p>
-                    </div>
                   </div>
                 </CardBack>
               </ReactCardFlip>
