971345fcc1365b0fefb143963b14a6876a84d001
yagmurcetintas <yagmurcetin@gmail.com>
2021-01-08 14:52:14 -0500

    optimized file sizes

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,18 +1,9 @@
 import React from "react"
 import Layout from "../components/Layout"
-import styles from "./gallery.module.scss"
 import Polaroid from "../components/Polaroid"
-import MeInColor from "../images/gallery/digital_art/me_in_color.png"
-import Lines from "../images/gallery/digital_art/IMG_0274.png"
-import JustChoose from "../images/gallery/digital_art/IMG_0276.png"
-import SocialDistance from "../images/gallery/digital_art/IMG_0277.png"
-import NeuronsWalking from "../images/gallery/digital_art/IMG_0371.png"
-import ThisIsFine from "../images/main/img404.jpg"
-import Kitsune from "../images/gallery/clay/kitsune_yagmur.jpg"
-import StandingTall from "../images/gallery/clay/standingtall_yagmur-scaled.jpg"
-import Kaonashi from "../images/gallery/clay/kaonashi_yagmur.jpg"
-import WolfMask from "../images/gallery/clay/wolf_yagmur.png"
+import { clayWork, digitalWork, sections } from "../utility/artworks"
 import { createHaiku } from "../utility/functions"
+import styles from "./gallery.module.scss"
 
 const GalleryPage = () => {
   const [section, setSection] = React.useState("digital")
@@ -20,91 +11,6 @@ const GalleryPage = () => {
   const changeSection = str => {
     setSection(str)
   }
-
-  const sections = [
-    { handle: "digital", title: "Digital" },
-    { handle: "clay", title: "Clay" },
-    { handle: "ink", title: "Ink" },
-  ]
-
-  const digitalWork = [
-    {
-      name: "Serene",
-      year: "2020",
-      medium: "Digital art made by using Procreate.",
-      src: Lines,
-      imgAlt:
-        "digital illustration of power lines with weird colorful creatures",
-    },
-    {
-      name: "Just Choose",
-      year: "2020",
-      medium: "Digital art made by using Procreate.",
-      src: JustChoose,
-      imgAlt:
-        "digital illustration of a beach and road signs with weird creatures",
-    },
-    {
-      name: "Neuron Riot",
-      year: "2020",
-      medium: "Digital art made by using Procreate.",
-      src: NeuronsWalking,
-      imgAlt: "digital illustration of neurons walking",
-    },
-    {
-      name: "This is fine",
-      year: "2020",
-      medium: "Digital art made by using Procreate.",
-      src: ThisIsFine,
-      imgAlt:
-        "digital illustration of a character surrounded by little versions of himself.",
-    },
-    {
-      name: "Self Portrait",
-      year: "2020",
-      medium: "Digital art made by using Procreate.",
-      src: MeInColor,
-      imgAlt: "digital illustration of myself in color",
-    },
-    {
-      name: "Social Distance",
-      year: "2020",
-      medium: "Digital art made by using Procreate.",
-      src: SocialDistance,
-      imgAlt: "digital illustration of humanity in 2020 social distancing",
-    },
-  ]
-
-  const clayWork = [
-    {
-      name: "Kitsune",
-      year: "2019",
-      medium: "Cone 6 clay & underglaze.",
-      src: Kitsune,
-      imgAlt: "Clay mask of kitsune",
-    },
-    {
-      name: "Kaonashi",
-      year: "2019",
-      medium: "Cone 6 clay & underglaze.",
-      src: Kaonashi,
-      imgAlt: "Clay mask of kaonashi",
-    },
-    {
-      name: "Standing Tall",
-      year: "2019",
-      medium: "Cone 6 clay, underglaze & clear glaze.",
-      src: StandingTall,
-      imgAlt: "Small clay sculpture",
-    },
-    {
-      name: "Wolf",
-      year: "2019",
-      medium: "Cone 10 clay & underglaze.",
-      src: WolfMask,
-      imgAlt: "Clay wolf mask",
-    },
-  ]
 
   const renderContent = () => {
     if (section === "digital") {
@@ -124,7 +30,17 @@ const GalleryPage = () => {
         <div className={styles.gridContainer}>
           {clayWork.map((w, i) => (
             <div className={styles.card} key={`claywork-${i}`}>
-              <Polaroid name={w.name} year={w.year} medium={w.medium}>
+              <Polaroid
+                name={w.name}
+                year={w.year}
+                medium={w.medium}
+                studio={w.studio}
+                link={
+                  w.studio === "Crealde School of Art"
+                    ? "http://crealde.org/"
+                    : null
+                }
+              >
                 <img src={w.src} alt={w.imgAlt} />
               </Polaroid>
             </div>
