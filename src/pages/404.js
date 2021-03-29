4d072770effd8a90ad9446fa12a9eed1022a2f97
yagmurcetintas <yagmurcetin@gmail.com>
2021-03-29 13:38:27 -0400

    Changed 404 style

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,5 @@
 import React from "react"
 import Layout from "../components/Layout"
-import JumpingBoy from "../images/main/character404.png"
 import TiredAdult from "../images/main/img404.jpg"
 import { createHaiku } from "../utility/functions"
 import styles from "./404.module.scss"
@@ -15,21 +14,12 @@ const Page404 = () => {
           <div className={styles.contentWrapper}>
             <div className={styles.infoWrapper}>
               <div className={styles.header}>404</div>
-              <img
-                src={JumpingBoy}
-                alt="Pink leafy character jumping"
-                className={styles.jumpingChild}
-              />
               <div className={styles.text}>
                 Well, it looks like the content you're looking for is not here.
                 Maybe try something else?
               </div>
             </div>
-            <img
-              src={TiredAdult}
-              alt="404"
-              className={styles.big404Img}
-            />
+            <img src={TiredAdult} alt="404" className={styles.big404Img} />
           </div>
         </div>
       </div>
