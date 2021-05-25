3e9d15ff901f6fb5730dd81e92f42b577a39cce8
yagmurcetintas <yagmurcetin@gmail.com>
2021-05-25 11:04:53 -0400

    Added licensing information to the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,18 @@ const Footer = () => {
   return (
     <footer className={styles.footerContainer}>
       <div className={styles.infoContainer}>
-        <p>© 2019-{new Date().getFullYear()} Yagmur C. Tas</p>
+        <p>
+          This work is licensed under{" "}
+          <a
+            href="http://creativecommons.org/licenses/by-nc-nd/4.0/?ref=chooser-v1"
+            target="_blank"
+            rel="license noopener noreferrer"
+            style={{ display: "inline-block" }}
+          >
+            CC BY-NC-ND 4.0
+          </a>
+          .
+        </p>
       </div>
       <div className={styles.infoContainer}>
         <StarIcon className={styles.starIcon} />
